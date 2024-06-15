e1f88c732c67984cd5af1357d32133e39c376b06
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-06-15 12:57:39 +1000

    Added bg and image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,11 +6,9 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="text-white">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      I'm a web developer who loves learning new things.
     </h3>
   </div>
 </template>

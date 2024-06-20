1f565635679ec309d0b14538c9ae38c7d31336b4
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-06-20 20:24:40 +1000

    Deploy imporved menu

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -3,7 +3,7 @@
     <div class="absolute flex flex-col items-center p-2 left-4 h-screen">
       <nav>
         <ul class="flex flex-col justify-evenly h-screen mb-16 w-12 justify-center items-center">
-           <li class="relative selected-icon flex flex-col text-xs gap-2 h-8 w-8"> <RouterLink to="/" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'house']" class="icon"/>Home</RouterLink></li>
+           <li class="relative flex flex-col text-xs gap-2 h-8 w-8" :class="{' selected-icon ' : isSelectedIcon }" > <RouterLink to="/" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'house']" class="icon"/>Home</RouterLink></li>
             <li class=" relative flex flex-col text-xs gap-2 h-8 w-8 " ><RouterLink to="/projects" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'wrench']" class="icon"/>Projects</RouterLink></li>
             <li class="relative flex flex-col text-xs gap-2 h-8 w-8"><RouterLink to="/contact" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'address-book']" class="icon"/>Contact</RouterLink></li>
             <li class=" relative flex flex-col text-xs gap-2 h-8 w-8"><RouterLink to="/skills" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'book']" class="icon"/>Skills</RouterLink></li>
@@ -15,6 +15,17 @@
     </div>
 
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isSelectedIcon = computed(() => {
+  return route.path === '/projects';
+});
+</script>
 
 <style scoped>
 

0520aa4e2c3bdadcf46458b220b6ac4f6ac9faed
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2024-06-21 08:00:37 +1000

    Much imporved sidebar menu logic

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -3,12 +3,43 @@
     <div class="absolute flex flex-col items-center p-2 left-4 h-screen">
       <nav>
         <ul class="flex flex-col justify-evenly h-screen mb-16 w-12 justify-center items-center">
-           <li class="relative flex flex-col text-xs gap-2 h-8 w-8" :class="{' selected-icon ' : isSelectedIcon }" > <RouterLink to="/" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'house']" class="icon"/>Home</RouterLink></li>
-            <li class=" relative flex flex-col text-xs gap-2 h-8 w-8 " ><RouterLink to="/projects" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'wrench']" class="icon"/>Projects</RouterLink></li>
-            <li class="relative flex flex-col text-xs gap-2 h-8 w-8"><RouterLink to="/contact" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'address-book']" class="icon"/>Contact</RouterLink></li>
-            <li class=" relative flex flex-col text-xs gap-2 h-8 w-8"><RouterLink to="/skills" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'book']" class="icon"/>Skills</RouterLink></li>
-           <li class=" relative flex flex-col text-xs gap-2 h-8 w-8"> <RouterLink to="/" class="icon-link flex flex-col justify-center items-center"><font-awesome-icon :icon="['fas', 'paperclip']" class="icon"/>Resume</RouterLink></li>
-           
+            
+            <li v-for="link in navLinks" 
+                :key="link.path"
+                class="relative flex flex-col text-xs gap-2 h-8 w-8" 
+            > 
+                <RouterLink 
+                    v-if="!link.external"
+                    :to="link.path" 
+                    class="icon-link flex flex-col justify-center items-center"
+                    custom 
+                    v-slot="{ isActive, href, navigate}"
+                >
+                    <a 
+                     :href="href" 
+                     @click="navigate" 
+                      :class="{ 'selected-icon': isActive }"
+                    >
+
+                        <font-awesome-icon :icon="link.icon" class="icon"/>
+                            {{ link.name }}
+                    </a>
+                </RouterLink>
+                <!-- External links -->
+                <a
+                    v-else
+                    :href="link.path" 
+                    class="icon-link flex flex-col justify-center items-center"
+                    target="_blank"
+                    ref="nofollow noopener"
+                >
+                    <font-awesome-icon :icon="link.icon" class="icon"/>
+                            {{ link.name }}
+                </a>
+    
+
+
+            </li>
         </ul>
       </nav>
 
@@ -17,10 +48,20 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const route = useRoute();
+
+const navLinks = ref([
+    {name: "home", path: "/", icon:['fas', 'house'], external: false },
+    {name: "projects", path: "/projects", icon:['fas', 'wrench'], external: false },
+    {name: "contact", path: "/contact", icon:['fas', 'address-book'], external: false },
+    {name: "skills", path: "/skills", icon:['fas', 'book'], external: false },
+    {name: "resume", path: "https://www.canva.com/design/DAFL43CkwGc/MEZeGrUguCvW0eMkwx4xZw/view?utm_content=DAFL43CkwGc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink", icon:['fas', 'paperclip'], external: true },
+    
+])
 
 const isSelectedIcon = computed(() => {
   return route.path === '/projects';

0141a8815d0d5d43278fa94ddb7bb1cd8a4ae10c
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2025-05-30 12:37:40 +1000

    Vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: "/mrmaverick79.github.io/",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
